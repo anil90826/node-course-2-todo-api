86df9a17f5fdc002e62b01d8cf459a5f8bb8ef62
Anil Kumar <anilkumar.90826@gmail.com>
2018-05-02 12:28:56 -0500

    ADD get todos

--- server/server.js
+++ server/server.js
@@ -20,12 +20,19 @@ app.post('/todos', (req, res) =>{
 
     todo.save().then((doc) => {
         res.send(doc);
-    }).catch( (err) => { console.log(err); });
+    }).catch( (err) => { res.status(400).send(err) });
 });
 
-
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }).catch( (e) => res.status(400).send(e) );
+  });
 
 
 app.listen(3000, () =>{
     console.log(`Status: App started on port:3000`);
 });
+
+
+module.exports = { app };
